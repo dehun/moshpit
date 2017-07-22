c3c01dcb8fd7b0c9430af75cc8b9ac23a9fa4bca
Yuriy Netesov <yuriy.netesov@gmail.com>
2017-07-22 13:51:58 +0200

    test test test

--- src/test/scala/AppDbSpec.scala
+++ src/test/scala/AppDbSpec.scala
@@ -1,15 +1,18 @@
 import akka.actor.ActorSystem
 import akka.testkit.{ImplicitSender, TestActors, TestKit}
 import moshpit.{VClock, actors}
-import moshpit.actors.AppDbProxy
-import moshpit.actors.AppDb
-import org.scalatest.concurrent.ScalaFutures
-import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
+import moshpit.actors.{AppDb, AppDbProxy, InstanceMetaInfo}
+import org.scalatest.concurrent.{PatienceConfiguration, ScalaFutures}
+import org.scalatest.{BeforeAndAfterAll, Inside, Matchers, WordSpecLike}
 import org.scalatest.Matchers._
+
+import scala.concurrent.duration._
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
+import org.scalatest.time.{Seconds, Span}
 
 
 class AppDbSpec() extends TestKit(ActorSystem("appDbTest")) with ImplicitSender
-  with WordSpecLike with Matchers with BeforeAndAfterAll with ScalaFutures {
+  with WordSpecLike with Matchers with BeforeAndAfterAll with ScalaFutures with Inside with GeneratorDrivenPropertyChecks {
 
   override def afterAll(): Unit = TestKit.shutdownActorSystem(system)
 
@@ -19,10 +22,41 @@ class AppDbSpec() extends TestKit(ActorSystem("appDbTest")) with ImplicitSender
       appDbProxy.updateInstance("so_app_id", "so_instance_guid", "wow")
       whenReady(appDbProxy.queryInstance("so_app_id", "so_instance_guid")) {
         case AppDb.Messages.QueryInstance.Success(meta, data) =>
-          meta.appId shouldEqual "so_app_id"
-          meta.wasDeleted shouldEqual false
-          meta.instanceTtlSec shouldEqual 60
-          meta.vclock shouldEqual VClock(Map("so_random_guid" -> 1))
+          inside(meta) { case InstanceMetaInfo(vclock, _, wasDeleted, instanceTtlSec, appId) =>
+            appId shouldEqual "so_app_id"
+            wasDeleted shouldEqual false
+            instanceTtlSec shouldEqual 60
+            vclock shouldEqual VClock(Map("so_random_guid" -> 1))
+          }
+      }
+    }
+
+    "ping existing instance" in {
+      val appDbProxy = new AppDbProxy(system.actorOf(AppDb.props("so_random_guid", 60, 60, 1200)))
+      appDbProxy.updateInstance("so_app_id", "so_instance_guid", "wow")
+      whenReady(appDbProxy.pingInstance("so_app_id", "so_instance_guid")) {
+        case AppDb.Messages.PingInstance.Success() =>
+      }
+    }
+
+    "ping non existing instance" in {
+      val appDbProxy = new AppDbProxy(system.actorOf(AppDb.props("so_random_guid", 60, 60, 1200)))
+      appDbProxy.updateInstance("so_app_id", "so_instance_guid", "wow")
+      whenReady(appDbProxy.pingInstance("so_app_id", "not_so_instance_guid")) {
+        case AppDb.Messages.PingInstance.NotExists() =>
+      }
+    }
+
+    "query apps" in {
+      forAll { (k:Int) =>
+        val n = k.abs % 1000
+        val appDbProxy = new AppDbProxy(system.actorOf(AppDb.props("so_random_guid", 60, 60, 1200)))
+        for (id <- 0 until n) {
+          appDbProxy.updateInstance(id.toString, id.toString, s"data of $id")
+        }
+        whenReady(appDbProxy.queryApps(), PatienceConfiguration.Timeout(Span(10, Seconds))) { result =>
+          result.keySet should === (List.range(0, n).map(_.toString).toSet)
+        }
       }
     }
   }
